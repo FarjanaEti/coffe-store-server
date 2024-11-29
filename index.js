dde5998c682106c211b21b947173c34265c989b8
Farjana Eti <etimourakkhieti@gmail.com>
2024-11-29 19:00:41 +0600

    last commit

--- index.js
+++ index.js
@@ -54,6 +54,28 @@ async function run() {
       const result=await coffeeCollection.deleteOne(query)
       res.send(result)
     })
+    //update single coffe
+    app.put('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+          $set: req.body
+      }
+
+      const result = await coffeeCollection.updateOne(filter, updatedDoc, options )
+
+      res.send(result);
+  })
+
+
+    //update
+    app.get('/coffee/:id', async(req,res)=>{
+      const id=req.params.id
+      const query={_id: new ObjectId(id)} 
+      const result=await coffeeCollection.findOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
